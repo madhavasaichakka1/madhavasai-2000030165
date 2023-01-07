9b02a5e5969651180de32e39b1c5089735f0ccb0
madhavasaichakka1 <2000030165@kluniversity.in>
2023-01-07 09:59:46 +0530

    Intial commit

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'madhavasai-2000030165'`, () => {
+  it(`should have as title 'Space'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('madhavasai-2000030165');
+    expect(app.title).toEqual('Space');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('madhavasai-2000030165 app is running!');
+    expect(compiled.querySelector('.content span')?.textContent).toContain('Space app is running!');
   });
 });
